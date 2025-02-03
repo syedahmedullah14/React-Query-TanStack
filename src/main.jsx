212b5ec154ca311415e479fbc37ebad9b4c0a91d
Jaser <syedahmedullahjaser@gmail.com>
2025-02-03 16:05:05 +0530

    Initializing TanStack QueryClient+

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Products from './Products.jsx';
 import Product from './Product.jsx';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,8 @@ const router = createBrowserRouter([
         element: <Product />,
     },
 ]);
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
