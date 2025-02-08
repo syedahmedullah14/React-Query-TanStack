c8cac9ac64d3460c184b363945a46e81d558d54f
Jaser <syedahmedullahjaser@gmail.com>
2025-02-08 16:09:19 +0530

    Fetching product by Id

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,5 +1,32 @@
-const Product = () => {
-    return <div>Products</div>;
-};
+import {
+    useQuery,
+  } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom';
 
+
+  
+
+    const Product = () => {
+
+        const params= useParams();
+
+        const fetchProduct = async () => {
+            const response = await fetch('https://dummyjson.com/products/${params.productId}');
+            const data = await response.json();
+            return data.products;
+        };
+         
+        const Products = () =>{
+            const {
+            isLoading, 
+            error, 
+            data: products,
+            } = useQuery({ queryKey: ['product', params.productId], queryFn: fetchProduct, 
+            // staleTime: 10000,
+            })
+            return <div>Products</div>;
+
+
+};
+    }
 export default Product;
