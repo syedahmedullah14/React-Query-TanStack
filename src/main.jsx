2b2cd35c2c6182c3873d6d0f025fdae7a4fda289
Jaser <syedahmedullahjaser@gmail.com>
2025-02-08 15:59:13 +0530

    Adding devtools

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx';
 import Products from './Products.jsx';
 import Product from './Product.jsx';
 
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -33,6 +35,8 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+
     </QueryClientProvider>
    
 );
